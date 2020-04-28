3f38585d045a6b56cf959d77ef5957680f9ebe10
Andrew Wong <andy.h.wong@gmail.com>
2020-04-28 16:46:52 -0400

    20200428 at 1646 EST

--- awso.go
+++ awso.go
@@ -8,15 +8,25 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+//
+type Awso struct {
+	s3Service *s3.S3
+}
+
+//
+func New(s3Service *s3.S3) *Awso {
+	return &Awso{s3Service: s3Service}
+}
+
 ////
 //
 // Signed url from filename
 //
 ////
-func GetSignedUrl(s3Service *s3.S3, usrId string, filename string) string {
+func (o *Awso) GetSignedUrl(usrId string, filename string) string {
 	bucket := "docculi-image"
 	key := bucket + "/" + usrId + "/" + filename
-	req, _ := s3Service.GetObjectRequest(&s3.GetObjectInput{
+	req, _ := (*o).s3Service.GetObjectRequest(&s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(key),
 	})
